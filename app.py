40c6a6024c4cf4aaa99043210af8f93a52331ec4
Martinha006 <souzamarta12321@gmail.com>
2023-11-16 16:09:50 -0300

    ta dando certoooo

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ db = mysql.connector.connect(host="localhost", user="root", password="", databas
 cursor = db.cursor()
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/tela_login.html', methods=['POST'])
+# Rota para simular o clique no botão de login
+@app.route('/simulate_login')
+def simulate_login():
+    return render_template('tela_login.html')
+
+@app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
@@ -21,38 +26,87 @@ def login():
         data = cursor.fetchall()
         if data:
             # Credenciais válidas, redireciona para a página desejada (por exemplo, tela_livros.html)
-            return redirect('tela_livros.html')
+            return render_template('tela_livros.html', dados=data)
         else:
             # Credenciais inválidas, redireciona de volta para a página de login
-            return redirect('/')
-            
-@app.route('/tela_cad.html', methods=['POST'])
+            return render_template('tela_login.html')
+
+# Rota para simular o clique no botão de cadastro
+@app.route('/simulate_add')
+def simulate_add():
+    return render_template('tela_cad.html')
+
+@app.route('/add', methods=['POST'])
 def add():
     if request.method == 'POST':
         email = request.form['email']
-        senha = request.form['senha'] 
+        senha = request.form['senha']
         # Insere um novo registro no banco de dados
-        cursor.execute("INSERT INTO usuario (email, senha) VALUES (%s, %s)", (email, senha))
+        cursor.execute("INSERT INTO usuario (usu_email, usu_senha) VALUES (%s, %s)", (email, senha))
         db.commit()
-    return redirect('/')
-
-
-# @app.route('/update', methods=['POST'])
-# def update():
-#     if request.method == 'POST':
-#         id = request.form['id']
-#         novo_nome = request.form['novo_nome']
-#         # Atualiza o registro no banco de dados
-#         cursor.execute("UPDATE tabela SET nome = %s WHERE id = %s", (novo_nome, id))
-#         db.commit()
-#     return redirect('/')
-
-# @app.route('/delete/<string:id>', methods=['GET'])
-# def delete(id):
-#     # Deleta o registro do banco de dados
-#     cursor.execute("DELETE FROM tabela WHERE id = %s", (id,))
-#     db.commit()
-#     return redirect('/')
+    return redirect('tela_livros.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+# # # Configuração do banco de dados
+# db = mysql.connector.connect(host="localhost", user="root", password="", database="livraria")
+# cursor = db.cursor()
+
+# @app.route('/')
+# def bla():
+#     return render_template('tela_livros.html')
+
+# # @app.route('/a')
+# # def teste():
+# #     return "aaaah"
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         senha = request.form['senha']
+#         # Verifica as credenciais no banco de dados
+#         cursor.execute("SELECT * FROM usuario WHERE usu_email=%s AND usu_senha=%s", (email, senha))
+#         data = cursor.fetchall()
+#         if data:
+#             # Credenciais válidas, redireciona para a página desejada (por exemplo, tela_livros.html)
+#             return render_template('tela_livros.html', dados=data)
+#         else:
+#             # Credenciais inválidas, redireciona de volta para a página de login
+#             return render_template('/tela_and.htnl')
+                
+
+# @app.route('/add', methods=['POST'])
+# def add():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         senha = request.form['senha'] 
+#         # Insere um novo registro no banco de dados
+#         cursor.execute("INSERT INTO usuario (usu_email, usu_senha) VALUES (%s, %s)", (email, senha))
+#         db.commit()
+#     return redirect('/')
+
+
+# # @app.route('/update', methods=['POST'])
+# # def update():
+# #     if request.method == 'POST':
+# #         id = request.form['id']
+# #         novo_nome = request.form['novo_nome']
+# #         # Atualiza o registro no banco de dados
+# #         cursor.execute("UPDATE tabela SET nome = %s WHERE id = %s", (novo_nome, id))
+# #         db.commit()
+# #     return redirect('/')
+
+# # @app.route('/delete/<string:id>', methods=['GET'])
+# # def delete(id):
+# #     # Deleta o registro do banco de dados
+# #     cursor.execute("DELETE FROM tabela WHERE id = %s", (id,))
+# #     db.commit()
+# #     return redirect('/')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- carrinho.py
+++ carrinho.py
@@ -1,41 +1,41 @@
-from flask import Flask, request, render_template, redirect
-import mysql.connector
+# from flask import Flask, request, render_template, redirect
+# import mysql.connector
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# Configuração do banco de dados
-db = mysql.connector.connect(host="localhost", user="root", password="", database="livraria")
-cursor = db.cursor()
-@app.route('/')
+# # Configuração do banco de dados
+# db = mysql.connector.connect(host="localhost", user="root", password="", database="livraria")
+# cursor = db.cursor()
+# @app.route('/')
 
-@app.route('/')
-def index():
-    return render_template('tela_and.html') # , id="login"
+# @app.route('/')
+# def index():
+#     return render_template('tela_and.html') # , id="login"
 
-@app.route('/index.html', methods=['POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['senha']
-        # Verifica as credenciais no banco de dados
-        cursor.execute("SELECT * FROM usuario WHERE usu_email=%s AND usu_senha=%s", (email, senha))
-        data = cursor.fetchall()
-        if data:
-            # Credenciais válidas, redireciona para a página desejada (por exemplo, tela_livros.html)
-            return redirect('tela_livros.html')
-        else:
-            # Credenciais inválidas, redireciona de volta para a página de login
-            return redirect('/')
+# @app.route('/index.html', methods=['POST'])
+# def login():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         senha = request.form['senha']
+#         # Verifica as credenciais no banco de dados
+#         cursor.execute("SELECT * FROM usuario WHERE usu_email=%s AND usu_senha=%s", (email, senha))
+#         data = cursor.fetchall()
+#         if data:
+#             # Credenciais válidas, redireciona para a página desejada (por exemplo, tela_livros.html)
+#             return redirect('tela_livros.html')
+#         else:
+#             # Credenciais inválidas, redireciona de volta para a página de login
+#             return redirect('/')
         
-@app.route('/tela_cad.html', methods=['POST'])
-def add():
-    if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['senha'] 
-        # Insere um novo registro no banco de dados
-        cursor.execute("INSERT INTO usuario (usu_email, usu_senha) VALUES (%s, %s)", (email, senha))
-        db.commit()
-    return redirect('/tela_livros.hmtl')
+# @app.route('/tela_cad.html', methods=['POST'])
+# def add():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         senha = request.form['senha'] 
+#         # Insere um novo registro no banco de dados
+#         cursor.execute("INSERT INTO usuario (usu_email, usu_senha) VALUES (%s, %s)", (email, senha))
+#         db.commit()
+#     return redirect('/tela_livros.hmtl')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
