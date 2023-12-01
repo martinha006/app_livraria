5493d87dbbaf0caac3d63ce7aa8193d3daff19a8
Martinha006 <souzamarta12321@gmail.com>
2023-12-01 12:04:07 -0300

    TEM QUE CONFERIR TUDO DENOVO

--- app.py
+++ app.py
@@ -3,19 +3,16 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# Configuração do banco de dados
+# CONFIGURAÇÃO DO BD
 db = mysql.connector.connect(host="localhost", user="root", password="", database="livraria")
 cursor = db.cursor()
 
+#  DIRECIONA PARA A TELA INICIAL // INDEX
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Rota para simular o clique no botão de login
-@app.route('/simulate_login')
-def simulate_login():
-    return render_template('tela_login.html')
-
+# TELA LOGIN // SELECT
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
@@ -33,6 +30,7 @@ def login():
             # Credenciais inválidas, redireciona de volta para a página de login
             return render_template('tela_login.html')
 
+# TELA CADASTRO // INSERT
 @app.route('/add', methods=['POST'])
 def add():
     if request.method == 'POST':
@@ -44,14 +42,7 @@ def add():
         prt = cursor.fetchall()
         return render_template('/tela_livros.html', produto=prt)
 
-@app.route('/simulate_compra')
-def simulate_compra():
-    titulo = request.form['titulo']
-    valor = request.form['valor']
-    cursor.execute("SELECT * FROM produto WHERE pr_preco = %s, pr_titulo = %s", (valor,titulo))
-    prt = cursor.fetchall()
-    return render_template('crrinho.html', produto=prt) # TA ERRADO
-
+# TELA DETALHAR
 @app.route('/simulate_det', methods=['POST'])
 def simulate_det():
     titulo = request.form['titulo']
@@ -60,24 +51,22 @@ def simulate_det():
     return render_template('detalhar.html', produto=prt)
 
 # MENU LATERAL
-@app.route('/simulate_and')
-def simulate_and():
-    return render_template('tela_and.html')
+@app.route('/livros')
+def livros():
+    return render_template('tela_livros.html')
 
-@app.route('/simulate_categoria', methods=['POST'])
-def simulate_categoria():
+@app.route('/ategoria', methods=['POST'])
+def categoria():
         cursor.execute("SELECT * FROM categoria")
         cat = cursor.fetchall()
         return render_template('/categoria.html', categoria=cat)
 
-@app.route('/simulate_top3')
+@app.route('/top3')
 def simulate_top3():
-    # Lógica para lidar com a rota do Top3 aqui
     return render_template('tela_and.html')
 
-@app.route('/simulate_configuracoes')
+@app.route('/configuracoes')
 def simulate_configuracoes():
-    # Lógica para lidar com a rota de configurações aqui
     return render_template('tela_and.html')
 
 if __name__ == '__main__':
